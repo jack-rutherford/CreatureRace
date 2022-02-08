9ae6abc23e77bdfdfead993698614f5426df1ce6
Jack Rutherford <jack.rutherford@hope.edu>
2022-02-08 10:34:51 -0500

    Added RaceInterface code and added them into Race

--- src/race/Race.java
+++ src/race/Race.java
@@ -2,4 +2,40 @@ package race;
 
 public class Race implements RaceInterface {
 
+	@Override
+	public char[] getRacetrack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRacerName(int racerIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getRacerPosition(int racerIndex) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean getRacerIsWinner(int racerIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createRace(int length, int numRacers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void advanceOneTurn() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/race/RaceInterface.java
+++ src/race/RaceInterface.java
@@ -1,5 +1,17 @@
 package race;
 
 public interface RaceInterface {
-
+	//Returns a char array representing the racetrack
+	char[] getRacetrack();
+	//Returns the name of the racer at the given index
+	String getRacerName(int racerIndex);
+	//Returns the position of the racer at the given index
+	int getRacerPosition(int racerIndex);
+	//Returns whether or not the racer at the given index is a winner
+	boolean getRacerIsWinner(int racerIndex);
+	//Creates the racetrack of the given length and instantiates ‘numRacers’ number of racers
+	void createRace(int length, int numRacers);
+	//Causes each racer to take one turn, moving a number of spaces based on their
+	// movement speed and the terrain
+	void advanceOneTurn();
 }
