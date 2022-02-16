5b904cee8d270f97441ab766488079545e6510c1
Jack Rutherford <jack.rutherford@hope.edu>
2022-02-15 20:36:13 -0500

    Worked on Race class

--- src/race/Race.java
+++ src/race/Race.java
@@ -7,6 +7,7 @@ public class Race implements RaceInterface {
 	private char[] racetrack;
 	private static char[] terrain;
 	private Random rand;
+	private Creature[] racers;
 
 
 	public Race(){
@@ -42,7 +43,7 @@ public class Race implements RaceInterface {
 		racetrack = new char[length];
 		for(int i = 0; i < racetrack.length; i++) {
 			int num1 = rand.nextInt(4);
-			if(racetrack[i] == racetrack[racetrack.length-1]) {
+			if(i == racetrack.length-1) {
 				racetrack[i] = terrain[4]; //finish line
 			}
 			else {
@@ -50,8 +51,24 @@ public class Race implements RaceInterface {
 			}
 		}
 
-		System.out.println(racetrack);
+		createRacers(numRacers);
 
+	}
+	
+	public void createRacers(int numRacers) {
+		racers = new Creature[numRacers];
+		for(int i = 0; i < racers.length; i++) {
+			int num1 = rand.nextInt(3);
+			if(num1 == 0) {
+				racers[i] = new Monkey("Name", racetrack);
+			}
+			else if(num1 == 1) {
+				racers[i] = new Ostrich("Name", racetrack);
+			}
+			else {
+				racers[i] = new Turtle("Name", racetrack);
+			}
+		}
 	}
 
 	@Override
